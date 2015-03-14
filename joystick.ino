8b1c3ee107d8764df1c0b30ce6c6383a021762d6
Vaicel <sarumanlol34@gmail.com>
2015-03-14 17:33:41 +0600

    Отрисовование двухмерного массива

--- joystick.ino
+++ joystick.ino
@@ -11,6 +11,19 @@
 #define SS_PIN 10
 #define COLS_NUM 8
 
+boolean pic [8][8]{
+
+{0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0},
+{0,1,0,0,1,0,0,0},
+{0,0,0,0,0,0,0,0},
+{0,0,0,1,0,0,0,0},
+{0,0,0,0,0,0,0,0},
+{0,0,0,0,0,1,0,0},
+{0,0,0,0,1,0,0,0}
+};
+
+byte x;
 
 void setup()
 {
@@ -20,41 +33,18 @@ void setup()
 }
 int p;
 
-/*void loop()
-{
-	if(analogRead(X_AXE_PIN)>RIGHT_THRESHOLD){
-		digitalWrite(p, LOW);
-		while(analogRead(X_AXE_PIN)>RIGHT_THRESHOLD){
-			digitalWrite(5, HIGH);
-			p = 5;
-		}
-	}
-	if(analogRead(Y_AXE_PIN)>UP_THRESHOLD){
-		digitalWrite(p, LOW);
-		while(analogRead(Y_AXE_PIN)>UP_THRESHOLD){
-			digitalWrite(6, HIGH);
-			p = 6;	
-		}
-	}
-	if(analogRead(X_AXE_PIN)<LEFT_THRESHOLD){
-		digitalWrite(p, LOW);
-		while(analogRead(X_AXE_PIN)<LEFT_THRESHOLD){
-			digitalWrite(7, HIGH);
-			p = 7;
-		}
-	}
-	if(analogRead(Y_AXE_PIN)<DOWN_THRESHOLD){
-		digitalWrite(p, LOW);
-		while(analogRead(Y_AXE_PIN)<DOWN_THRESHOLD){
-			digitalWrite(8, HIGH);
-			p = 8;
-		}
-	}
-}*/
 void loop()
 	{
-		digitalWrite(SS_PIN,LOW);
-		SPI.transfer(0xFF^ 1<<map(analogRead(A1), 0, 1022, 0, 7));
-		SPI.transfer(1<<map(analogRead(A0), 0, 1021, 0, 7));	
-		digitalWrite(SS_PIN,HIGH);
+		for(int t = 0; t<8; t++){
+			digitalWrite(SS_PIN,LOW);
+			SPI.transfer(0xFF ^ 1<<t);
+			x=0;
+			for(int v = 7; v>=0; v--){
+				x = x+(pic[t][v] << v);	
+			}
+			SPI.transfer(x);
+			digitalWrite(SS_PIN,HIGH);
+			delay(1);
+		}
+
 	}	
