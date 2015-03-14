130edf40c7c5bbdf38b9ce8a1d5d0af9427c057b
Илья Дубков <dubkov@skbrii.ru>
2015-03-15 01:52:23 +0600

    добавил структуру

--- joystick.ino
+++ joystick.ino
@@ -1,11 +1,11 @@
 #include <SPI.h>
+#include "snake.h"
 
 #define X_AXE_PIN A0
 #define Y_AXE_PIN A1
 
 #define UP_THRESHOLD 900
 #define DOWN_THRESHOLD 200
-
 #define LEFT_THRESHOLD 200
 #define RIGHT_THRESHOLD 900
 
@@ -14,8 +14,13 @@
 
 #define DRAW_TIME 100
 
-int headX=0;
-int headY=6;
+//struct Point{
+//   int x;
+//   int y;
+//};
+
+//int headX=0;
+//int headY=6;
 
 int dir = 1;
 // 0 - up
@@ -23,28 +28,9 @@ int dir = 1;
 // 2 - left
 // 3 - right
 
-boolean pic[8][8] = {NULL};
+boolean pic[8][8] ;//= {NULL};
 
 int timer = 0, timePrev = 0;
-
-void setup()
-{
-	SPI.begin();
-  	pinMode(SS_PIN, OUTPUT);
-	pinMode(4, INPUT_PULLUP);
-}
-int p;
-
-void loop()
-{
-	dir = 1;
-	timer=millis();
-	if((timer - timePrev) >= DRAW_TIME){
-		generateHead(headX, headY, dir);
-		timePrev=timer;
-	}
-	draw();
-}
 
 void draw(){
 	int col;
@@ -59,13 +45,34 @@ void draw(){
 	}
 }
 
-void generateHead(int headX, int headY, int dir){
+Point generateHead(Point head, int dir){
 	switch(dir){
-		case 1: 	headY += 1; break;
-		case 0: 	headY -= 1; break;
-		case 2: 	headX -= 1; break;
-		case 3: 	headX += 1; break;
+		case 1: 	head.y += 1; break;
+		case 0: 	head.y -= 1; break;
+		case 2: 	head.x -= 1; break;
+		case 3: 	head.x += 1; break;
 	}
 	pic[8][8]=NULL;
-	pic[headY][headX]=1;
+	pic[head.y][head.x]=1;
+	return head;
 }	
+
+void setup()
+{
+	SPI.begin();
+  	pinMode(SS_PIN, OUTPUT);
+	pinMode(4, INPUT_PULLUP);
+}
+
+void loop()
+{
+	Point head={1,1};
+	dir = 1;
+	timer=millis();
+	if((timer - timePrev) >= DRAW_TIME){
+		head = generateHead(head, dir);
+		timePrev=timer;
+	}
+	draw();
+}
+
