ad543b29a993716b6aef637a7a3b53d478405779
Vaicel <sarumanlol34@gmail.com>
2015-04-01 15:07:31 +0600

    XBocT

--- joystick.ino
+++ joystick.ino
@@ -10,6 +10,7 @@ boolean pic[8][8];
 int timer = 0, timerPrev = 0;
 
 int snakeLength=3;
+int lastdir = 5;
 
 Snake head = {0,2,1};
 Snake body[62] = {NULL};
@@ -39,16 +40,26 @@ void draw(boolean pic[8][8]){
 Snake generateHead(Snake head){
 	head.dir = getDir(head.dir);
 	switch(head.dir){
-		case 1: 	head.y += 1; break;
-		case 0: 	head.y -= 1; break;
-		case 2: 	head.x -= 1; break;
-		case 3: 	head.x += 1; break;
+		case 1: 	if(lastdir !=0){
+					head.y += 1;}
+					else{head.y -=1;} break;
+		case 0: 	if(lastdir !=1){
+					head.y -= 1;}
+					else{head.y +=1;} break;
+		case 2: 	if(lastdir !=3){
+					head.x -= 1;}
+					else{head.x +=1;} break;
+		case 3: 	if(lastdir !=2){
+					head.x += 1;}
+					else{head.x -=1;} break;
 	}
 	pic[head.y][head.x] = 1;
 	return head;
+	lastdir = head.dir;
 }
 
 Snake generateBody(Snake body[62]){
+	clearMatrix();
 	for(int gbi = snakeLength-2; gbi >= 1; gbi--){
 		body[gbi]=body[gbi-1];
 		pic[body[gbi].y][body[gbi].x] = 1;
